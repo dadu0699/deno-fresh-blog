870d80e439c86c7a26f69a2ae33ecad5f9148897
Didier Domínguez <dadu0699@hotmail.com>
2023-03-25 21:11:12 -0600

    refactor: Styles

--- layouts/Footer.tsx
+++ layouts/Footer.tsx
@@ -2,11 +2,11 @@ export default function Footer() {
   const links = [{ name: "Home", url: "/" }];
 
   return (
-    <footer class="mx-auto my-16 pb-6 space-y-4 text-center text-sm md:text-base text-gray-400 dark:text-gray-600">
+    <footer class="mx-auto my-16 pb-6 space-y-3 text-center text-sm md:text-base text-gray-600 dark:text-gray-500">
       <ul class="flex items-center justify-center">
         {links.map(({ name, url }) => (
           <>
-            <li class="text-sm md:text-base text-gray-600 dark:text-gray-400">
+            <li class="text-sm md:text-base text-gray-700 dark:text-gray-500">
               <a
                 class="hover:text-gray-800 dark:hover:text-gray-200 hover:underline"
                 href={url}
