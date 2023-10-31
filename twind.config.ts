e365cfeb70a4169866324e717015218b00ad570f
Didier Domínguez <dadu0699@hotmail.com>
2023-10-31 12:56:31 -0600

    build: Twind update

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,11 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import { Options } from "$fresh/plugins/twindv1.ts";
+import { defineConfig, Preset } from "@twind/core";
+import presetAutoprefix from "@twind/preset-autoprefix";
+import presetTailwind from "@twind/preset-tailwind";
 
 export default {
+  ...defineConfig({
+    presets: [presetTailwind() as Preset, presetAutoprefix()],
+  }),
   selfURL: import.meta.url,
-} as Options;
+} as unknown as Options;
